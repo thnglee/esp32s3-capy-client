97d56c6be7f9d33d5ba0956ae58cd70d00cc571f
vietct513 <89295573+vietct513@users.noreply.github.com>
2025-09-24 16:00:46 +0700

    config for mini board

--- main/boards/bread-compact-wifi/config.h
+++ main/boards/bread-compact-wifi/config.h
@@ -1,3 +1,4 @@
+
 #ifndef _BOARD_CONFIG_H_
 #define _BOARD_CONFIG_H_
 
@@ -10,6 +11,31 @@
 #define AUDIO_I2S_METHOD_SIMPLEX
 
 #ifdef AUDIO_I2S_METHOD_SIMPLEX
+  // === MIC INMP441 (theo dây của bạn) ===
+  #define AUDIO_I2S_MIC_GPIO_WS    GPIO_NUM_2   // WS/LRCLK
+  #define AUDIO_I2S_MIC_GPIO_SCK   GPIO_NUM_3   // SCK/BCLK
+  #define AUDIO_I2S_MIC_GPIO_DIN   GPIO_NUM_4   // SD -> ESP
+
+  // === LOA MAX98357A (theo dây của bạn) ===
+  #define AUDIO_I2S_SPK_GPIO_DOUT  GPIO_NUM_5   // ESP -> DIN (amp)
+  #define AUDIO_I2S_SPK_GPIO_BCLK  GPIO_NUM_6   // BCLK
+  #define AUDIO_I2S_SPK_GPIO_LRCK  GPIO_NUM_7   // LRCLK/WS
+
+  // (bridge macro – nhiều file codec dùng tên tổng quát)
+  #define AUDIO_I2S_GPIO_BCLK  AUDIO_I2S_SPK_GPIO_BCLK
+  #define AUDIO_I2S_GPIO_WS    AUDIO_I2S_SPK_GPIO_LRCK
+  #define AUDIO_I2S_GPIO_DOUT  AUDIO_I2S_SPK_GPIO_DOUT
+  #define AUDIO_I2S_GPIO_DIN   AUDIO_I2S_MIC_GPIO_DIN
+
+#else
+  // DUPLEX (không dùng vì bạn đang đấu 2 bộ clock khác nhau)
+  #define AUDIO_I2S_GPIO_WS    GPIO_NUM_7
+  #define AUDIO_I2S_GPIO_BCLK  GPIO_NUM_6
+  #define AUDIO_I2S_GPIO_DIN   GPIO_NUM_4
+  #define AUDIO_I2S_GPIO_DOUT  GPIO_NUM_5
+#endif
+
+/*#ifdef AUDIO_I2S_METHOD_SIMPLEX
 
 #define AUDIO_I2S_MIC_GPIO_WS   GPIO_NUM_4
 #define AUDIO_I2S_MIC_GPIO_SCK  GPIO_NUM_5
@@ -25,17 +51,23 @@
 #define AUDIO_I2S_GPIO_DIN  GPIO_NUM_6
 #define AUDIO_I2S_GPIO_DOUT GPIO_NUM_7
 
-#endif
+#endif*/
 
+// LED + Buttons (ESP32-S3 Super Mini)
+#define BUILTIN_LED_GPIO          GPIO_NUM_12   // nếu không có LED: dùng GPIO_NUM_NC
+#define BOOT_BUTTON_GPIO          GPIO_NUM_0    // nút BOOT có sẵn trên board
+#define TOUCH_BUTTON_GPIO         GPIO_NUM_1    // WAKE
+#define VOLUME_UP_BUTTON_GPIO     GPIO_NUM_10   // Vol+
+#define VOLUME_DOWN_BUTTON_GPIO   GPIO_NUM_11   // Vol-
 
-#define BUILTIN_LED_GPIO        GPIO_NUM_48
+/*#define BUILTIN_LED_GPIO        GPIO_NUM_48
 #define BOOT_BUTTON_GPIO        GPIO_NUM_0
 #define TOUCH_BUTTON_GPIO       GPIO_NUM_47
 #define VOLUME_UP_BUTTON_GPIO   GPIO_NUM_40
-#define VOLUME_DOWN_BUTTON_GPIO GPIO_NUM_39
+#define VOLUME_DOWN_BUTTON_GPIO GPIO_NUM_39*/
 
-#define DISPLAY_SDA_PIN GPIO_NUM_41
-#define DISPLAY_SCL_PIN GPIO_NUM_42
+#define DISPLAY_SDA_PIN GPIO_NUM_8
+#define DISPLAY_SCL_PIN GPIO_NUM_9
 #define DISPLAY_WIDTH   128
 
 #if CONFIG_OLED_SSD1306_128X32
